bddb748ad2bd2616e9c70c2af67e909608086c62
王浩然 <309568486@qq.com>
2017-05-22 00:18:11 +0800

    模块控制器，支持了输入变量

--- web/smartyModController.php
+++ web/smartyModController.php
@@ -6,6 +6,10 @@
  * Time: 下午11:19
  */
 abstract class kod_web_smartyModController{
+    public $assignValueList = array();
     abstract function init($aData);
     abstract function finish($aData);
+    public function assign($k,$v){
+        $this->assignValueList[$k] = $v;
+    }
 }
