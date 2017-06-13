fb8437a91f72e84bf1080adbe26f893fe94997b5
王浩然 <309568486@qq.com>
2017-06-13 20:41:05 +0800

    修改了demo

--- demo/singleEntryInclude.php
+++ demo/singleEntryInclude.php
@@ -5,62 +5,59 @@
  * Date: 2017/3/20
  * Time: 下午2:17
  */
-ini_set('display_errors', 1);
-error_reporting(E_ALL ^ E_NOTICE);
-date_default_timezone_set('PRC');
-/*
-*设置kod环境变量
-*/
-define("webDIR",dirname(__FILE__)."/http/");//网站根目录
-define("KOD_SMARTY_TEMPLATE_DIR",dirname(__FILE__)."/http/");
-define("KOD_REWRITE_HTML_LINK",true);
-define('KOD_SMARTY_COMPILR_DIR',dirname(__FILE__).'/html/smarty_cache');
-
-define('KOD_MYSQL_SERVER','118.190.95.219');
-define('KOD_MYSQL_USER','root');
-define('KOD_MYSQL_PASSWORD','1082322$%&whr309568486');
-define('KOD_COMMENT_MYSQLDB','dbName');
-
-define("KOD_SMARTY_CSS_DIR",dirname(__FILE__)."/cssCreate/");
-define('KOD_SMARTY_CSS_HOST','http://118.190.95.234/androidCreate/cssCreate/');
-
-
-
-spl_autoload_register(function($model){
-    $classAutoLoad = array(
-        'projectClass' => 'include/project.php',
-        'mem' => 'include/mem.php',
-        'pvClass' => 'include/pv.php',
-        'articleTypeClass' => 'include/articleType.php',
-        'articleClass' => 'include/article.php',
-        'webObjectbase' => 'include/webObjectbase.php',
-    );
-    if(isset($classAutoLoad[$model])){
-        include_once $classAutoLoad[$model];
+include_once('include.php');
+final class kod_web_rewrite extends kod_tool_config{
+    public static function getArrByStr($str){
+        $splitStr = '/';
+        $arr = explode($splitStr,$str);
+        return $arr;
     }
-});
-/*
- * 进行rewrite规则
-*/
-kod_web_rewrite::init(dirname(__FILE__)."/rewrite.conf");
-$path = kod_web_rewrite::getPathByUrl($_SERVER["SCRIPT_URL"].$_SERVER["QUERY_STRING"]);
+    public static function getPageContent($confPath)
+    {
+        $file = fopen($confPath, "r") or exit("Unable to open file!");
+        $lineList = array();
+        $before = array();
+        while(!feof($file)) {
+            $oneLine = fgets($file);
+            $oneArr = explode(" ",$oneLine);
+            $oneArr[count($oneArr)-1] = trim($oneArr[count($oneArr)-1]);
+            if(count($oneArr)==1){
+                if(substr($oneArr[0],0,2)=="\t\t"){
+                    $before = array($before[0],$before[1],trim($oneArr[0]));
+                }elseif(substr($oneArr[0],0,1)=="\t"){
+                    $before = array($before[0],trim($oneArr[0]));
+                }else{
+                    $before = array(trim($oneArr[0]));
+                }
+                continue;
+            }elseif(count($oneArr)==0){
+                continue;
+            }
+            $oneArr[0] = implode('',$before).trim($oneArr[0]);
+            $lineList[] = $oneArr;
 
-$houzhui = explode('.',$_SERVER["SCRIPT_URL"]);
-$houzhui = end($houzhui);
-if($houzhui=='css'){
-    header("Content-Type: text/css");
-}elseif(in_array($houzhui,array('css','png','jpeg','jpg','gif'))){
-    header("Content-Type: image/".$houzhui);
+        }
+        fclose($file);
+        return $lineList;
+    }
 }
-if($path!=""){
-    $new = parse_url($path);
+kod_web_rewrite::init(dirname(__FILE__).'/rewrite.conf');
+//双向获取
+$result = kod_web_rewrite::getPathByUrl($_SERVER["SCRIPT_URL"]);
+if(!empty($result)){
+    $new = parse_url(KOD_SMARTY_TEMPLATE_DIR.$result[1]);
+    parse_str($new["query"],$myArray);
+    $_GET = array_merge($_GET,$myArray);//后面盖住前面
     $_SERVER["SCRIPT_URL"] = $new["path"];
     $_SERVER["SCRIPT_URI"] = "http://".$_SERVER["HTTP_HOST"].$new["path"];
-    $_SERVER["REQUEST_URI"] = $new["path"]."?".$new["query"];
-    $_SERVER["SCRIPT_FILENAME"] = dirname(__FILE__)."/http".$new["path"];
+    if(!empty($new["query"])){
+        $_SERVER["REQUEST_URI"] = $new["path"]."?".$new["query"];
+    }else{
+        $_SERVER["REQUEST_URI"] = $new["path"];
+    }
+    $_SERVER["SCRIPT_FILENAME"] = $new["path"];
     $_SERVER["SCRIPT_NAME"] = $new["path"];
     $_SERVER["PHP_SELF"] = $new["path"];
-
     $columnList = explode("&",$new["query"]);
     $_GET = array();
     foreach($columnList as $column){
@@ -78,19 +75,14 @@ if($path!=""){
         }
         $_REQUEST[$temp[0]] = $temp[1];
     }
-    unset($path);
+    unset($result);
     unset($new);
     if(substr($_SERVER["SCRIPT_FILENAME"],strlen($_SERVER["SCRIPT_FILENAME"])-1,1)=="/"){
         include_once($_SERVER["SCRIPT_FILENAME"]."index.php");
     }else{
-
         include_once($_SERVER["SCRIPT_FILENAME"]);
     }
 }else{
-    $_SERVER["SCRIPT_FILENAME"] = webDIR.$_SERVER["SCRIPT_URL"];
-    if(substr($_SERVER["SCRIPT_FILENAME"],strlen($_SERVER["SCRIPT_FILENAME"])-1,1)=="/"){
-        $_SERVER["SCRIPT_FILENAME"] = $_SERVER["SCRIPT_FILENAME"]."index.php";
-    }
-    include_once($_SERVER["SCRIPT_FILENAME"]);exit;
-    echo "404";exit;
+    header("HTTP/1.1 404 Not Found");
+    var_dump('404');exit;
 }
