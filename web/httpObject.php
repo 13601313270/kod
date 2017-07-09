e3e2ae01765f4ad8952cffe8e06f8e22f1397dd9
王浩然 <309568486@qq.com>
2017-07-09 23:51:48 +0800

    kod_web_httpObject类内部改为了直接使用更为方便的page对象

--- web/httpObject.php
+++ web/httpObject.php
@@ -29,6 +29,7 @@ abstract class kod_web_httpObject{
 		);
 	}
 	final public function run($allowMethod_="GET,POST"){//$_GET $_POST $_COOKIE $_REQUEST $_FILES
+		$this->smarty = new kod_web_page();
 		$allowMethod = explode(",",$allowMethod_);
 		$allParams = array();//所有网址输入的参数值
 		foreach($allowMethod as $k){
@@ -63,30 +64,14 @@ abstract class kod_web_httpObject{
 				}
 			}
 		}
-		$this->smarty = $this->initSmarty();
-		$this->assign("allParams",$funParam);//将网址的所有参数传入网页
 		if($method->getNumberOfParameters()>0){
 			$method->invokeArgs($this,$funParam);
 		}else{
 			$method->invoke($this);
 		}
 		if(!empty($this->smartyTpl)){
-			try{
-				$this->beforeRun();
-				$this->smarty->fetch($this->smartyTpl, null, null, null, true);
-			}catch(Exception $e){
-				switch($e->getCode()){
-					case 1:
-						throw new Exception("smarty无法对预编译文件进行存储，请尝试在【".get_called_class()."】类中定义一个initSmarty方法，initSmarty第一个参数是关联的smarty的对象，对这个参数传入的smarty对象赋予compile_dir属性值。也可以在kod框架加载文件中配置变量【KOD_SMARTY_COMPILR_DIR】作为默认值",0,$e);
-						break;
-					case 2:
-						throw new Exception("【".get_called_class()."】类设置的smartyTpl属性值【{$this->smartyTpl}】，文件无法找到",0,$e);
-						break;
-					default :
-						throw new Exception("其他错误",0,$e);
-						break;
-				}
-			}
+			$this->beforeRun();
+			$this->smarty->fetch($this->smartyTpl);
 		}else{
 			throw new Exception("请给【".get_called_class()."】类设置smartyTpl属性用来指定对应tpl文件");
 		}
@@ -94,21 +79,8 @@ abstract class kod_web_httpObject{
 			echo $v;
 		}
 	}
-	public function initSmarty(){
-		$smartyObject = new kod_smarty_smarty();
-		if(count($this->smartyPlutPath)>=1){
-			foreach($this->smartyPlutPath as $k=>$v){
-				$smartyObject->addPluginsDir($v);
-			}
-		}
-		$smartyObject->compile_dir = KOD_SMARTY_COMPILR_DIR;//设置编译目录
-		//$smartyObject->template_dir = KOD_DIR_NAME."/testRun/";//设置模板目录
-		//$smartyObject->config_dir = "smarty/templates/config";//目录变量
-		//$smartyObject->cache_dir = "smarty/templates/cache"; //缓存文件夹
-		return $smartyObject;
-	}
 	public function assign($key,$val){
-		$this->smarty->assign($key,$val);
+		$this->smarty->$key = $val;
 	}
 
 }
